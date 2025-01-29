50417a7d4fecb76cf18d7f69dff9c566a052ebf0
Suraj0950 <surajsuryaveer@gmail.com>
2025-01-29 12:16:41 +0530

    learning node with sql database

---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- Active: 1737914816693@@127.0.0.1@3306@delta_app
+CREATE TABLE user (
+	id VARCHAR(50) PRIMARY KEY,
+	username VARCHAR(50) UNIQUE,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL
+);
+
+
